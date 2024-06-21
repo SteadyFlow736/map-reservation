34134d2d7167333b66e50b7b3720606924741624
junseung <junseung736@gmail.com>
2024-06-21 21:33:57 +0900

    create project

---
+++ src/main/java/org/example/mapreservation/MapReservationApplication.java
@@ -0,0 +1,13 @@
+package org.example.mapreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mapreservation/MapReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mapreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
