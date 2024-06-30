5459e029764540110a0f268e040ab3687d8839ed
junseung <junseung736@gmail.com>
2024-06-30 22:21:06 +0900

    존재하지 않는 고객 아이디 제출 시나리오 테스트 추가

--- src/test/java/org/example/mapreservation/auth/LoginTest.java
+++ src/test/java/org/example/mapreservation/auth/LoginTest.java
@@ -1,5 +1,7 @@
 package org.example.mapreservation.auth;
 
+import org.assertj.core.api.Assertions;
+import org.example.mapreservation.customer.domain.Customer;
 import org.example.mapreservation.customer.dto.CustomerCreateRequest;
 import org.example.mapreservation.customer.repository.CustomerRepository;
 import org.example.mapreservation.customer.service.CustomerService;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -62,12 +66,35 @@ public class LoginTest {
         String password = "12345678";
         customerService.createCustomer(new CustomerCreateRequest(email, password));
 
-        // when - 일치하지 않는 비밀번호
-        // then - 로그인 성공
+        // when - 일치하지 않는 비밀번호로 로그인 시도
+        // then - 로그인 실패
         String wrongPassword = password + "123zxc";
         mockMvc.perform(post("/api/login")
                         .param("username", email)
                         .param("password", wrongPassword)
+                        .with(csrf())
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(ErrorCode.AUTH_INVALID_USERNAME_OR_PASSWORD.name()))
+                .andExpect(jsonPath("$.message").value(ErrorCode.AUTH_INVALID_USERNAME_OR_PASSWORD.getMessage()))
+                .andExpect(jsonPath("$.errors").isEmpty());
+    }
+
+    @DisplayName("존재하지 않는 고객 아이디 제출 시 로그인 실패")
+    @Test
+    void givenNotExistingCustomer_thenLoginFails() throws Exception {
+        // given - 가입되지 않은 고객
+        String email = "abc@gmail.com";
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        Assertions.assertThat(optionalCustomer).isEmpty();
+
+        // when - 가입되지 않은 고객으로 로그인 시도
+        // then - 로그인 실패
+        String password = "12345678";
+        mockMvc.perform(post("/api/login")
+                        .param("username", email)
+                        .param("password", password)
                         .with(csrf())
                 )
                 .andDo(MockMvcResultHandlers.print())
