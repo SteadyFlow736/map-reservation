2329a72a723536d248e04a019390b294f9e7a567
junseung <junseung736@gmail.com>
2024-07-01 20:57:15 +0900

    로그아웃 성공 핸들러 추가

--- src/main/java/org/example/mapreservation/config/SecurityConfig.java
+++ src/main/java/org/example/mapreservation/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -20,6 +21,7 @@ public class SecurityConfig {
 
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
     private final AuthenticationFailureHandler authenticationFailureHandler;
+    private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,11 +31,17 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/csrf-token").permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(Customizer.withDefaults())
+                .csrf(Customizer.withDefaults()) // TODO: CSRF 인증 실패 응답 커스터마이징 하기
                 .formLogin(customizer -> customizer
+                        // UsernamePasswordAuthenticationFilter에서 로그인 진행
                         .loginProcessingUrl("/api/login")
                         .successHandler(authenticationSuccessHandler)
                         .failureHandler(authenticationFailureHandler)
+                )
+                .logout(customizer -> customizer
+                        // LogoutFilter에서 로그아웃 진행
+                        .logoutUrl("/api/logout")
+                        .logoutSuccessHandler(logoutSuccessHandler)
                 )
         ;
         return http.build();
---
+++ src/test/java/org/example/mapreservation/auth/LogoutTest.java
@@ -0,0 +1,43 @@
+package org.example.mapreservation.auth;
+
+import org.example.mapreservation.customer.service.CustomerService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class LogoutTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    CustomerService customerService;
+
+    @DisplayName("고객은 로그아웃할 수 있다.")
+    @WithMockUser(username = "abc@gmail.com")
+    @Test
+    void givenLogout_thenSuccess() throws Exception {
+        // when - 로그아웃 수행
+        mockMvc.perform(post("/api/logout")
+                        .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+        // then - 인증이 필요한 곳에 접근이 불가하다.
+        mockMvc.perform(post("/api/secured")
+                        .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+}
