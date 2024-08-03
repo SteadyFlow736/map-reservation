c4fc3c36385b2e54fa90fb888a4aa5f23d83ae7f
junseung <junseung736@gmail.com>
2024-08-03 19:02:26 +0900

    헤어샵 예약 버전 관리 테스트 추가

--- src/test/java/org/example/mapreservation/reservation/service/ReservationServiceTest.java
+++ src/test/java/org/example/mapreservation/reservation/service/ReservationServiceTest.java
@@ -12,6 +12,7 @@ import org.example.mapreservation.reservation.domain.HairShopReservation;
 import org.example.mapreservation.reservation.dto.HairShopReservationCreateRequest;
 import org.example.mapreservation.reservation.repository.HairShopReservationRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -70,6 +74,49 @@ class ReservationServiceTest {
         assertThat(foundReservation.get().getHairShop().getId()).isEqualTo(hairShop.getId());
         assertThat(foundReservation.get().getCustomer().getId()).isEqualTo(customer.getId());
         assertThat(foundReservation.get().getReservationTime()).isEqualTo(reservationTime);
+    }
+
+    @DisplayName("예약 성공하면 헤어샵의 version이 1 증가한다.")
+    @Test
+    void givenReservationSucceeds_thenHairShopVersionIncreases() {
+        // given
+        int version = hairShop.getVersion();
+        LocalDateTime reservationTime = LocalDateTime.of(2024, Month.APRIL, 10, 13, 30);
+        LocalDateTime currentTime = reservationTime.minusHours(2);
+        HairShopReservationCreateRequest request = new HairShopReservationCreateRequest(reservationTime);
+
+
+        // when
+        reservationService.createHairShopReservation(hairShop.getId(), customer.getEmail(), currentTime, request);
+
+        // then
+        HairShop updatedHairShop = hairShopRepository.findById(hairShop.getId()).orElseThrow();
+        assertThat(updatedHairShop.getVersion()).isEqualTo(version + 1);
+    }
+
+    // TODO: 데드락 발생! 원인 찾고 해결하기!
+    @Test
+    void test() throws InterruptedException {
+        // given
+        LocalDateTime reservationTime = LocalDateTime.of(2024, Month.APRIL, 10, 13, 30);
+        LocalDateTime currentTime = reservationTime.minusHours(2);
+        HairShopReservationCreateRequest request = new HairShopReservationCreateRequest(reservationTime);
+
+        int threadCount = 3;
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        ExecutorService es = Executors.newFixedThreadPool(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            es.submit(() -> {
+                try {
+                    reservationService.createHairShopReservation(hairShop.getId(), customer.getEmail(), currentTime, request);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
     }
 
 }
