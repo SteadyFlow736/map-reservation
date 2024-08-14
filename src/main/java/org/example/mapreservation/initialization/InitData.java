dd27f9da11ecd5d80afeafbb3ae3b27b1fcd0056
junseung <junseung736@gmail.com>
2024-08-14 14:39:34 +0900

    backend-fix: 초기 데이터 설정 빈 프로파일을 local로 지정하여 테스트 시 초기 데이터 설정되지 못하도록 제한

--- src/main/java/org/example/mapreservation/initialization/InitData.java
+++ src/main/java/org/example/mapreservation/initialization/InitData.java
@@ -2,8 +2,10 @@ package org.example.mapreservation.initialization;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("local")
 @RequiredArgsConstructor
 @Component
 public class InitData {
