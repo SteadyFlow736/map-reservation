86490178e4405236b73de7cf66263ff48b418f7d
junseung <junseung736@gmail.com>
2024-08-17 13:18:07 +0900

    frontend-fix: 맵 좌표 표시 로직을 useEffect 없이 계산하여 성능 향상

--- front/src/components/MainMap.tsx
+++ front/src/components/MainMap.tsx
@@ -2,30 +2,28 @@ import {naver_map_client_id} from "@/config/map";
 import Script from "next/script";
 import {useAtomValue} from "jotai";
 import {hairShopSearchResultAtom} from "@/atoms";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+
+const markers: naver.maps.Marker[] = []
 
 function MainMap() {
     const [map, setMap] = useState<naver.maps.Map>()
-    const [markers, setMarkers] = useState<naver.maps.Marker[]>([])
     const hairShopSearchResult = useAtomValue(hairShopSearchResultAtom);
 
-    useEffect(() => {
-        if (map && hairShopSearchResult) {
-            markers.forEach(m => m.setMap(null)) // 기존 마커들을 맵에서 삭제
-            const newMarkers: naver.maps.Marker[] = [] // 새로운 마커 리스트
-
-            hairShopSearchResult?.content?.forEach(dto => {
-                const latitude = parseFloat(dto.latitude)
-                const longitude = parseFloat(dto.longitude)
-                const marker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(latitude, longitude),
-                    map
-                })
-                newMarkers.push(marker)
+    // marker 표시
+    markers.forEach(m => m.setMap(null))
+    markers.length = 0
+    if (map && hairShopSearchResult) {
+        hairShopSearchResult.content.forEach(dto => {
+            const latitude = parseFloat(dto.latitude)
+            const longitude = parseFloat(dto.longitude)
+            const marker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(latitude, longitude),
+                map
             })
-            setMarkers(newMarkers)
-        }
-    }, [map, hairShopSearchResult])
+            markers.push(marker)
+        })
+    }
 
     return (
         <>
