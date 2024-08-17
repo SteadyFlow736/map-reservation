ee70d72257af8ee382cb5a909b9ec13ca168587d
junseung <junseung736@gmail.com>
2024-08-17 16:41:09 +0900

    frontend-dependency: tanstack reactquery 추가 및 세팅

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,16 +3,18 @@
 import SearchColumn from "@/components/SearchColumn";
 import MainMap from "@/components/MainMap";
 import ShopDetailColumn from "@/components/ShopDetailColumn";
+import {QueryClientProvider,} from "@tanstack/react-query";
+import queryClient from "@/config/queryClient";
 
 function Home() {
     return (
-        <div>
+        <QueryClientProvider client={queryClient}>
             <div className="absolute flex gap-3">
                 <SearchColumn/>
                 <ShopDetailColumn/>
             </div>
             <MainMap/>
-        </div>
+        </QueryClientProvider>
     );
 }
 
