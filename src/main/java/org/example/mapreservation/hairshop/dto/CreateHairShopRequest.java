c0ae83969babcfb4571c16ddda8ad4ec5ef98aa5
junseung <junseung736@gmail.com>
2024-08-21 19:18:11 +0900

    backend-fix: CreateHairShopRequest 생성자 추가함으로써 기존 테스트 코드 HairShopServiceTest::saveHairShop 에러 제거

--- src/main/java/org/example/mapreservation/hairshop/dto/CreateHairShopRequest.java
+++ src/main/java/org/example/mapreservation/hairshop/dto/CreateHairShopRequest.java
@@ -5,9 +5,15 @@ import org.example.mapreservation.common.Address;
 import java.util.List;
 
 public record CreateHairShopRequest(String name, Address address, Long ownerId, List<String> imageUrls) {
-    public CreateHairShopRequest(String name, String roadAddress, String detailAddress, Long ownerId) {
-        this(name, new Address(roadAddress, detailAddress), ownerId, null);
+
+    public CreateHairShopRequest(String name, Address address, Long ownerId) {
+        this(name, address, ownerId, List.of());
     }
+
+    public CreateHairShopRequest(String name, String roadAddress, String detailAddress, Long ownerId) {
+        this(name, new Address(roadAddress, detailAddress), ownerId, List.of());
+    }
+
     public CreateHairShopRequest(String name, String roadAddress, String detailAddress, Long ownerId, List<String> imageUrls) {
         this(name, new Address(roadAddress, detailAddress), ownerId, imageUrls);
     }
