2c6908cf70bdcc1ee2678360be74d03150609038
junseung <junseung736@gmail.com>
2024-08-25 16:26:22 +0900

    backend-feature: 회원 가입 요청 DTO에 validation 적용하고 테스트

--- src/main/java/org/example/mapreservation/customer/dto/CustomerCreateRequest.java
+++ src/main/java/org/example/mapreservation/customer/dto/CustomerCreateRequest.java
@@ -1,9 +1,26 @@
 package org.example.mapreservation.customer.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.example.mapreservation.customer.domain.Customer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public record CustomerCreateRequest(String email, String password) {
+public record CustomerCreateRequest(
+        @Email(message = "이메일 형식이 잘못되었습니다.")
+        @NotBlank(message = "이메일은 필수 제출 항목입니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수 제출 항목입니다.")
+        @Size(min = 8, max = 16, message = "비밀번호 길이는 8자리 이상 16자리 이하여야 합니다.")
+        @Pattern(regexp = ".*[A-Z].*", message = "비밀번호는 적어도 하나 이상의 대문자를 포함해야 합니다.")
+        @Pattern(regexp = ".*[a-z].*", message = "비밀번호는 적어도 하나 이상의 소문자를 포함해야 합니다.")
+        @Pattern(regexp = ".*\\d.*", message = "비밀번호는 적어도 하나 이상의 숫자를 포함해야 합니다.")
+        @Pattern(regexp = ".*[@$!%*?&].*", message = "비밀번호는 적어도 다음 특수 문자(@$!%*?&) 중 하나 이상을 포함해야 합니다.")
+        String password
+) {
+
     public Customer toEntity(PasswordEncoder passwordEncoder) {
         String encodedPassword = passwordEncoder.encode(password);
         return new Customer(email, encodedPassword);
---
+++ src/test/java/org/example/mapreservation/customer/dto/CustomerCreateRequestTest.java
@@ -0,0 +1,97 @@
+package org.example.mapreservation.customer.dto;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CustomerCreateRequestTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenValidEmailAndPassword_thenNoConstraintViolations() {
+        String email = "abc@gmail.com";
+        String password = "Password1!";
+        CustomerCreateRequest request = new CustomerCreateRequest(email, password);
+
+        Set<ConstraintViolation<CustomerCreateRequest>> violations =
+                validator.validate(request);
+
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenInvalidEmail_thenConstraintViolation() {
+        String email = "invalid-email";
+        String password = "Password1!";
+        CustomerCreateRequest request = new CustomerCreateRequest(email, password);
+
+        Set<ConstraintViolation<CustomerCreateRequest>> violations =
+                validator.validateProperty(request, "email");
+
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("이메일 형식이 잘못되었습니다.");
+    }
+
+    @MethodSource("invalidPasswordProvider")
+    @ParameterizedTest
+    void whenInvalidPassword_thenConstraintViolation(String password, String violationMessage) {
+        String email = "abc@gmail.com";
+        CustomerCreateRequest request = new CustomerCreateRequest(email, password);
+
+        Set<ConstraintViolation<CustomerCreateRequest>> violations =
+                validator.validateProperty(request, "password");
+
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo(violationMessage);
+    }
+
+    static Stream<Arguments> invalidPasswordProvider() {
+        String lengthViolationMessage = "비밀번호 길이는 8자리 이상 16자리 이하여야 합니다.";
+        String upperCaseViolationMessage = "비밀번호는 적어도 하나 이상의 대문자를 포함해야 합니다.";
+        String lowerCaseViolationMessage = "비밀번호는 적어도 하나 이상의 소문자를 포함해야 합니다.";
+        String digitViolationMessage = "비밀번호는 적어도 하나 이상의 숫자를 포함해야 합니다.";
+        String specialCharacterViolationMessage = "비밀번호는 적어도 다음 특수 문자(@$!%*?&) 중 하나 이상을 포함해야 합니다.";
+
+        return Stream.of(
+                Arguments.of("Passd1!", lengthViolationMessage), // 7자리
+                Arguments.of("Password1!zzzzzzz", lengthViolationMessage), // 17자리
+                Arguments.of("password1!", upperCaseViolationMessage), // 대문자 없음
+                Arguments.of("PASSWORD1!", lowerCaseViolationMessage), // 소문자 없음
+                Arguments.of("Password!", digitViolationMessage), // 숫자 없음
+                Arguments.of("Password1", specialCharacterViolationMessage) // 특수문자 없음
+        );
+    }
+
+    @Test
+    void whenBlankPassword_thenAllConstraintViolations() {
+        String email = "abc@gmail.com";
+        String password = "";
+        CustomerCreateRequest request = new CustomerCreateRequest(email, password);
+
+        Set<ConstraintViolation<CustomerCreateRequest>> violations =
+                validator.validateProperty(request, "password");
+
+        assertThat(violations.size()).isEqualTo(6);
+    }
+
+}
