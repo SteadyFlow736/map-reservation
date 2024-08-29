00169e5baee938efe6479c6f71bef249351b07f7
Junseung Park <sylvan0212@gmail.com>
2024-08-29 23:01:10 +0900

    frontend-ui: 로그인 상태에 따라 유저 아이콘 색 변경 적용

--- front/src/app/user/page.tsx
+++ front/src/app/user/page.tsx
@@ -9,6 +9,8 @@ function UserPage() {
     const {status, user} = useAuth()
     if (status === 'loading') return <div>Loading</div>
     if (status === 'unauthenticated') {
+        // 페이지 히스토리 최상단을 user에서 login으로 변경하고 login으로 이동.
+        // login 페이지에서 로그인 포기하고 뒤로 가기 누르면 user 페이지가 아니라 전 화면(대표적으로 메인 화면)으로 돌아가기 위해서임
         router.replace('/login')
         return
     }
