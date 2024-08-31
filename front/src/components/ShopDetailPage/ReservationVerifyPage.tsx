37fd4e7be3e51f36c1a34ca239e19ee11d0b89a9
junseung <junseung736@gmail.com>
2024-08-31 12:16:36 +0900

    frontend-feature: 예약 확정 페이지 로그인 guard 추가

--- front/src/components/ShopDetailPage/ReservationVerifyPage.tsx
+++ front/src/components/ShopDetailPage/ReservationVerifyPage.tsx
@@ -2,11 +2,31 @@ import {useSetAtom} from "jotai/index";
 import {selectedHairShopIdAtom} from "@/atoms";
 import {XMarkIcon} from "@heroicons/react/16/solid";
 import {ArrowLeftIcon} from "@heroicons/react/24/outline";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {ShopMainPageContext, TimeSlotContext} from "@/components/ShopDetailPage/ShopDetailWrapperPage";
 import Time from "@/utils/Time";
+import {useRouter} from "next/navigation";
+import useAuth from "@/hooks/useAuth";
 
+/**
+ * 고객이 예약 하려는 내용을 확인하고 확정하는 페이지
+ */
 function ReservationVerifyPage() {
+    // TODO: 로그인을 요구하는 코드를 재활용 가능하게 만들기. user page에서도 같은 로직을 사용중이다.
+    const router = useRouter()
+    const {status, user} = useAuth()
+
+    useEffect(() => {
+        if (status === 'unauthenticated') {
+            // 페이지 히스토리 최상단을 user에서 login으로 변경하고 login으로 이동.
+            // login 페이지에서 로그인 포기하고 뒤로 가기 누르면 user 페이지가 아니라 전 화면(대표적으로 메인 화면)으로 돌아가기 위해서임
+            router.replace('/login')
+            return
+        }
+    }, [router, status]);
+
+    if (status === 'loading' || status === 'unauthenticated') return <div>Loading</div>
+
     return (
         <div className="bg-white">
             <StickyNavBar/>
@@ -16,6 +36,9 @@ function ReservationVerifyPage() {
     )
 }
 
+/**
+ * 상단 고정 nav 바
+ */
 function StickyNavBar() {
     const setSelectedHairShopId = useSetAtom(selectedHairShopIdAtom)
     const {setShopMainPage} = useContext(ShopMainPageContext)
@@ -36,6 +59,9 @@ function StickyNavBar() {
     )
 }
 
+/**
+ * 예약 날짜, 시간 등 예약 하려는 정보 표시
+ */
 function ReservationInfo() {
     const {selectedTimeSlot, setSelectedTimeSlot} = useContext(TimeSlotContext)
     if (!selectedTimeSlot) return null
@@ -51,6 +77,9 @@ function ReservationInfo() {
     );
 }
 
+/**
+ * 이전 페이지로 돌아가는 버튼, 현재 예약을 확정하는 버튼
+ */
 function Buttons() {
     const {setShopMainPage} = useContext(ShopMainPageContext)
 
