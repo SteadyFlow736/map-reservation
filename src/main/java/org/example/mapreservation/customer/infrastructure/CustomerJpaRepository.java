ce3b71f668a3290403a569e4b9833a6ead798064
junseung <junseung736@gmail.com>
2024-10-12 15:20:47 +0900

    test: CustomerJpaRepository 테스트 추가

--- src/main/java/org/example/mapreservation/customer/infrastructure/CustomerJpaRepository.java
+++ src/main/java/org/example/mapreservation/customer/infrastructure/CustomerJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerJpaRepository extends JpaRepository<Customer, Long> {
-    Optional<Customer> findByEmail(String username);
+    Optional<Customer> findByEmail(String email);
 }
---
+++ src/test/java/org/example/mapreservation/customer/domain/CustomerTest.java
@@ -0,0 +1,8 @@
+package org.example.mapreservation.customer.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+}
