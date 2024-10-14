e1fe5542f6fe3a77056246498492e2bc1b7b185e
junseung <junseung736@gmail.com>
2024-10-14 15:07:09 +0900

    refactor: Reservation 패키지 구조 변경

--- src/main/java/org/example/mapreservation/reservation/application/HairShopReservationServiceImpl.java
+++ src/main/java/org/example/mapreservation/reservation/application/HairShopReservationServiceImpl.java
@@ -38,18 +38,9 @@ public class HairShopReservationServiceImpl implements HairShopReservationServic
     private final RedissonClient redissonClient;
     private final TransactionTemplate transactionTemplate;
 
-    /**
-     * 헤어샵 예약
-     *
-     * @param shopId          대상 헤어샵 id
-     * @param username        고객명(이메일)
-     * @param currentDateTime 현재 시간
-     * @param request         요청 내용(예약 시간)
-     * @return 예약 id
-     */
     @Override
-    public HairShopReservationCreateResponse createReservation(Long shopId, String username, LocalDateTime currentDateTime,
-                                                               HairShopReservationCreateRequest request) {
+    public HairShopReservationCreateResponse createReservation(
+            Long shopId, String username, LocalDateTime currentDateTime, HairShopReservationCreateRequest request) {
 
         // redisson을 이용한 분산락 적용으로 "헤어샵 id + 예약 시간" 조합이 유일하게 예약될 수 있도록 함
         // TODO: 분산락을 재사용 가능하게, 깔끔하게 적용할 수 있는 법 찾기. (wrapping method or aop or facade ...?)
@@ -64,7 +55,8 @@ public class HairShopReservationServiceImpl implements HairShopReservationServic
             if (!available) {
                 throw new CustomException(ErrorCode.LCK_CANNOT_ACQUIRE_LOCK, "잠시 후 다시 시도해 주세요.");
             }
-            Long reservationId = transactionTemplate.execute(status -> createHairShopReservationInternal(shopId, username, currentDateTime, request));
+            Long reservationId = transactionTemplate.execute(status ->
+                    createReservationInternal(shopId, username, currentDateTime, request));
             return new HairShopReservationCreateResponse(reservationId);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -73,8 +65,52 @@ public class HairShopReservationServiceImpl implements HairShopReservationServic
         }
     }
 
-    private Long createHairShopReservationInternal(Long shopId, String username, LocalDateTime currentDateTime,
-                                                   HairShopReservationCreateRequest request) {
+    @Transactional
+    @Override
+    public ReservationStatus getReservationStatus(Long shopId, HairShopReservationStatusGetRequest request) {
+        LocalDateTime searchStartDateTime = request.getStartDateTime();
+        LocalDateTime searchEndDateTime = request.getEndDateTime();
+
+        HairShop hairShop = hairShopRepository.findById(shopId)
+                .orElseThrow(() -> new CustomException(ErrorCode.HS_NOT_FOUND));
+
+        List<HairShopReservation> reservations = hairShopReservationRepository
+                .findByHairShopAndReservationTimeBetween(hairShop, searchStartDateTime, searchEndDateTime);
+
+        // TODO: 영업 시작, 종료 시간은 헤어샵 마다, 기준 날짜마다 다르게 가져갈 수 있도록 기능 넣기. 일단 고정된 값으로 전달.
+        LocalTime openingTime = LocalTime.of(10, 0);
+        LocalTime closingTime = LocalTime.of(20, 0);
+        return ReservationStatus.from(request.targetDate(), openingTime, closingTime, reservations);
+    }
+
+    @Transactional
+    @Override
+    public HairShopReservationResponse getReservation(Long reservationId, String username) {
+        HairShopReservation hairShopReservation = hairShopReservationRepository.findByIdAndCustomerEmail(reservationId, username)
+                .orElseThrow(() -> new CustomException(ErrorCode.HSR_NOT_FOUND));
+        return HairShopReservationResponse.from(hairShopReservation);
+    }
+
+    @Transactional
+    @Override
+    public Slice<HairShopReservationResponse> getReservations(String username, Pageable pageable) {
+        Slice<HairShopReservation> slice = hairShopReservationRepository.findByCustomerEmail(username, pageable);
+        // TODO: HairShopReservation -> HairShopReservationDto 변경 시 Customer 엔티티 조회 추가로 일어나는 N + 1 문제 해결
+        // ReesrvationServiceTest::getHairShopReservations 테스트에서 N + 1 문제 확인 가능
+        return slice.map(HairShopReservationResponse::from);
+    }
+
+    @Transactional
+    @Override
+    public void cancelReservation(Long reservationId, String username, LocalDateTime currentDateTime) {
+        HairShopReservation hairShopReservation = hairShopReservationRepository.findByIdAndCustomerEmail(reservationId, username)
+                .orElseThrow(() -> new CustomException(ErrorCode.HSR_NOT_FOUND));
+
+        hairShopReservation.cancel(currentDateTime);
+    }
+
+    private Long createReservationInternal(
+            Long shopId, String username, LocalDateTime currentDateTime, HairShopReservationCreateRequest request) {
 
         isValidReservationTime(currentDateTime, request.reservationTime());
 
@@ -108,71 +144,6 @@ public class HairShopReservationServiceImpl implements HairShopReservationServic
         if (reservationTime.getSecond() != 0 || reservationTime.getNano() != 0) {
             throw new CustomException(ErrorCode.HSR_INVALID_RESERVATION_TIME);
         }
-    }
-
-    @Transactional
-    @Override
-    public ReservationStatus getReservationStatus(Long shopId, HairShopReservationStatusGetRequest request) {
-        LocalDateTime searchStartDateTime = request.getStartDateTime();
-        LocalDateTime searchEndDateTime = request.getEndDateTime();
-
-        HairShop hairShop = hairShopRepository.findById(shopId)
-                .orElseThrow(() -> new CustomException(ErrorCode.HS_NOT_FOUND));
-
-        List<HairShopReservation> reservations = hairShopReservationRepository
-                .findByHairShopAndReservationTimeBetween(hairShop, searchStartDateTime, searchEndDateTime);
-
-        // TODO: 영업 시작, 종료 시간은 헤어샵 마다, 기준 날짜마다 다르게 가져갈 수 있도록 기능 넣기. 일단 고정된 값으로 전달.
-        LocalTime openingTime = LocalTime.of(10, 0);
-        LocalTime closingTime = LocalTime.of(20, 0);
-        return ReservationStatus.from(request.targetDate(), openingTime, closingTime, reservations);
-    }
-
-    /**
-     * 헤어샵 예약 정보 조회
-     *
-     * @param reservationId 헤어샵 예약 id
-     * @param username      조회 요청 유저
-     * @return 헤어샵 예약 정보
-     */
-    @Transactional
-    @Override
-    public HairShopReservationResponse getReservation(Long reservationId, String username) {
-        HairShopReservation hairShopReservation = hairShopReservationRepository.findByIdAndCustomerEmail(reservationId, username)
-                .orElseThrow(() -> new CustomException(ErrorCode.HSR_NOT_FOUND));
-        return HairShopReservationResponse.from(hairShopReservation);
-    }
-
-    /**
-     * 고객별 예약 정보 조회 (slicing)
-     *
-     * @param username 고객 이메일
-     * @param pageable 페이지 정보
-     * @return 헤어샵 예약 정보 (slice)
-     */
-    @Transactional
-    @Override
-    public Slice<HairShopReservationResponse> getReservations(String username, Pageable pageable) {
-        Slice<HairShopReservation> slice = hairShopReservationRepository.findByCustomerEmail(username, pageable);
-        // TODO: HairShopReservation -> HairShopReservationDto 변경 시 Customer 엔티티 조회 추가로 일어나는 N + 1 문제 해결
-        // ReesrvationServiceTest::getHairShopReservations 테스트에서 N + 1 문제 확인 가능
-        return slice.map(HairShopReservationResponse::from);
-    }
-
-    /**
-     * 예약 취소
-     *
-     * @param reservationId   예약 id
-     * @param username        예약자(고객 이메일)
-     * @param currentDateTime 현재 시간
-     */
-    @Transactional
-    @Override
-    public void cancelReservation(Long reservationId, String username, LocalDateTime currentDateTime) {
-        HairShopReservation hairShopReservation = hairShopReservationRepository.findByIdAndCustomerEmail(reservationId, username)
-                .orElseThrow(() -> new CustomException(ErrorCode.HSR_NOT_FOUND));
-
-        hairShopReservation.cancel(currentDateTime);
     }
 
 }
--- src/main/java/org/example/mapreservation/reservation/application/service/HairShopReservationService.java
+++ src/main/java/org/example/mapreservation/reservation/application/service/HairShopReservationService.java
@@ -11,16 +11,52 @@ import org.springframework.data.domain.Slice;
 import java.time.LocalDateTime;
 
 public interface HairShopReservationService {
-    HairShopReservationCreateResponse createReservation(Long shopId,
-                                                        String username,
-                                                        LocalDateTime currentDateTime,
-                                                        HairShopReservationCreateRequest request);
 
+    /**
+     * 헤어샵 예약 생성
+     *
+     * @param shopId          대상 헤어샵 id
+     * @param username        고객명(이메일)
+     * @param currentDateTime 현재 시간
+     * @param request         요청 내용(예약 시간)
+     * @return 예약 id
+     */
+    HairShopReservationCreateResponse createReservation(
+            Long shopId, String username, LocalDateTime currentDateTime, HairShopReservationCreateRequest request);
+
+    /**
+     * 헤어샵 하루 예약 현황 조회
+     *
+     * @param shopId  헤어샵 id
+     * @param request 날짜
+     * @return 헤어샵 예약 현황
+     */
     ReservationStatus getReservationStatus(Long shopId, HairShopReservationStatusGetRequest request);
 
+    /**
+     * 헤어샵 예약 상세 조회
+     *
+     * @param reservationId 헤어샵 예약 id
+     * @param username      조회 요청 유저
+     * @return 헤어샵 예약 정보
+     */
     HairShopReservationResponse getReservation(Long reservationId, String username);
 
+    /**
+     * 유저 예약 히스토리 조회 (slicing)
+     *
+     * @param username 조회 요청 유저
+     * @param pageable 페이지 정보
+     * @return 헤어샵 예약 정보 (slice)
+     */
     Slice<HairShopReservationResponse> getReservations(String username, Pageable pageable);
 
+    /**
+     * 예약 취소
+     *
+     * @param reservationId   예약 id
+     * @param username        유저(예약자)
+     * @param currentDateTime 현재 시간
+     */
     void cancelReservation(Long reservationId, String username, LocalDateTime currentDateTime);
 }
