1a69d671d838bc0f311b862e27d43d3ec23a7852
junseung <junseung736@gmail.com>
2025-01-20 17:33:32 +0900

    [frontend] fix: 검색 결과가 없을 시 검색 결과 없음을 알려주는 문구 출력

--- front/src/components/SearchColumn/SearchResultList.tsx
+++ front/src/components/SearchColumn/SearchResultList.tsx
@@ -2,13 +2,23 @@ import StoreSummaryCard from "@/components/StoreSummaryCard";
 import {hairShopSearchResponseAtom} from "@/atoms";
 import {useAtomValue} from "jotai";
 
+function NoSearchResult() {
+    return (
+        <div className="p-4">검색어와 일치하는 헤어샵이 없습니다.</div>
+    );
+}
+
 function SearchResultList() {
     const hairShopSearchResponse = useAtomValue(hairShopSearchResponseAtom)
     const data = hairShopSearchResponse?.page.content
+    const dataExist = data !== undefined && data.length > 0;
 
     return (
         <div className="overflow-y-auto divide-y">
-            {data?.map(s => <StoreSummaryCard key={s.shopId} storeSummary={s}/>)}
+            {dataExist ?
+                data.map(s => <StoreSummaryCard key={s.shopId} storeSummary={s}/>) :
+                <NoSearchResult/>
+            }
         </div>
     )
 }
